ce259833628fa492f00604fb6fb11afba824a390
Juzet Arana <arana.juzet@gmail.com>
2015-05-15 08:38:09 -0700

    finished 12/13

--- index.php
+++ index.php
@@ -47,6 +47,29 @@
 			savePictures($image_url);
 		}
 	}
+		// function to print out images onto screen 
+	function printImages ($userID) {
+		$url = 'https://api.instagram.com/v1/users/'.$userID.'/media/recent?client_id='.clientID.'&count=5';
+		$instagramInfo = connectToInstagram($url);
+		$results = json_decode($instagramInfo, true);
+		// Parse through the info one by one
+		foreach($results['data'] as $items){
+			$image_url = $items['images']['low_resolution']['url']; //going through all the results and giving myself back the url of all the pictures because we want to save it in the php server
+			echo '<img src =" '. $image_url .' "/><br/>';
+			// calling a function to save $image_url
+			savePictures($image_url);
+		}
+	}
+	// functions to save image to server
+	function savePictures($image_url) {
+		echo $image_url.'<br>';
+		$filename = basename($image_url);//filename is what we are storing, basename is the PHP built in method that we are using to store $image_url
+		echo $filename . '<br>';
+
+		$destination = ImageDirectory. $filename; //making sure the images doesnt ecist in the storage
+		file_put_contents($destination, file_get_contents($image_url)); //gooes and grabs an imagefile and stores it into our server
+	}
+
 	// checking for get
 	if (isset($_GET['code'])) {
 		$code = ($_GET['code']);
